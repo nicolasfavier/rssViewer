ed2d6aada110354a768e1f81545bb55b6f882213
nicolasfavier <niko.favier@gmail.com>
2014-11-05 16:20:51 +0100

    fix the format requested

--- src/main/java/servlet/SynthetiseurFluxServlet.java
+++ src/main/java/servlet/SynthetiseurFluxServlet.java
@@ -31,12 +31,12 @@ public class SynthetiseurFluxServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-    	String[] rssTab = {"http://www.lemondeinformatique.fr/flux-rss/thematique/toutes-les-actualites/rss.xml"};
-    	rssTab[1] = "";
+    	String[] rssTab = {"http://www.lemondeinformatique.fr/flux-rss/thematique/toutes-les-actualites/rss.xml",
+    			"http://wxdata.weather.com/wxdata/weather/rss/local/FRXX0055?cm_ven=LWO&cm_cat=rss"};
+  
     	
 		String format = "xml";
 		String result = "";
-
 		PrintWriter out = response.getWriter();
 
 		if (request.getParameter("format") != null) {
@@ -45,16 +45,16 @@ public class SynthetiseurFluxServlet extends HttpServlet {
 
 		response.setCharacterEncoding("UTF-8");
 
-		if (format == "xml") {
+		if (format.equals("xml")) {
 			response.setContentType("text/xml");
 			result = FormatManager.getXMLFromRss(rssTab);
 		}
-		else if (format == "html"){
+		else if (format.equals("html")){
 			response.setContentType("text/html");
 			result = FormatManager.getHTML(rssTab);	
 		}
 
-		else if(format == "json"){
+		else if(format.equals("json")){
 			response.setContentType("text/json");
 			result = FormatManager.getJson(rssTab);			
 		}		
