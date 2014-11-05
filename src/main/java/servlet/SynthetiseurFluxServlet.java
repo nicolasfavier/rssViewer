a2d9ef8ab6cd114f4295cc9abb8236e776de915e
nicolasfavier <niko.favier@gmail.com>
2014-11-05 17:25:22 +0100

    post

--- src/main/java/servlet/SynthetiseurFluxServlet.java
+++ src/main/java/servlet/SynthetiseurFluxServlet.java
@@ -63,4 +63,42 @@ public class SynthetiseurFluxServlet extends HttpServlet {
 		out.println(result);
 
 	}
+    
+    protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+    	//String[] rssTab;
+    	//rssTab= request.getParameterValues("rssTab");
+    	
+    	
+    	String[] rssTab = {"http://www.lemondeinformatique.fr/flux-rss/thematique/toutes-les-actualites/rss.xml"};
+    	
+    	
+		String format = "xml";
+		String result = "";
+		PrintWriter out = response.getWriter();
+
+		if (request.getParameter("format") != null) {
+			format = request.getParameter("format");
+		}
+
+		response.setCharacterEncoding("UTF-8");
+
+		if (format.equals("xml")) {
+			response.setContentType("text/xml");
+			result = FormatManager.getXMLFromRss(rssTab);
+		}
+		else if (format.equals("html")){
+			response.setContentType("text/html");
+			result = FormatManager.getHTML(rssTab);	
+		}
+
+		else if(format.equals("json")){
+			response.setContentType("text/json");
+			result = FormatManager.getJson(rssTab);			
+		}		
+
+		out.println(result);
+
+	}
 }
